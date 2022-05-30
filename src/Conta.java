f04fea3d42d88fd3b33e7973c689e393111d2afe
LuisaGPurificacao <luisa.purificacao16@gmail.com>
2022-05-30 12:55:25 -0300

    Polymorphism - account number and agency methods and attributes added

--- src/Conta.java
+++ src/Conta.java
@@ -1,27 +1,36 @@
 
-public abstract class Conta implements IConta{
-	
+public abstract class Conta implements IConta {
+
 	// ATRIBUTOS
-	private int agencia;
-	private int conta;
-	private double saldo;
+	protected int agencia;
+	protected int conta;
+	protected double saldo;
+
+	private static final int AGENCIA_PADRAO = 0;
+	private static int SEQUENCIAL = 1;
+
+	// MÉTODOS CONSTRUTORES
+	public Conta() {
+		this.agencia = Conta.AGENCIA_PADRAO;
+		this.conta = SEQUENCIAL++;
+	}
 
 	// MÉTODOS DE AÇÃO
 	@Override
 	public void sacar(double valor) {
-		
+
 	}
 
 	@Override
 	public void depositar(double valor) {
-		
+
 	}
 
 	@Override
 	public void transferir(double valor, Conta contaDestino) {
-		
+
 	}
-	
+
 	// MÉTODOS GETS E SETS
 	public int getAgencia() {
 		return agencia;
