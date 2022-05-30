9cf5ac529cfbb34d0b8bd68bb1804805aebfde92
LuisaGPurificacao <luisa.purificacao16@gmail.com>
2022-05-30 12:24:31 -0300

    Added the classes. Added the basic atributtes and methods

---
+++ src/ContaCorrente.java
@@ -0,0 +1,19 @@
+
+public class ContaCorrente {
+
+	int agencia;
+	int conta;
+	double saldo;
+
+	public void sacar() {
+
+	}
+
+	public void depositar() {
+
+	}
+
+	public void transferir() {
+
+	}
+}
---
+++ src/ContaPoupanca.java
@@ -0,0 +1,19 @@
+
+public class ContaPoupanca {
+
+	int agencia;
+	int conta;
+	double saldo;
+	
+	public void sacar() {
+		
+	}
+	
+	public void depositar() {
+		
+	}
+	
+	public void transferir() {
+		
+	}
+}
